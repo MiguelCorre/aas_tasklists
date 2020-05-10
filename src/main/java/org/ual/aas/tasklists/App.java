22692861ece965601f204f6265fc8561caac9fdc
MiguelCorre <miguelahpc@hotmail.com>
2020-05-10 19:26:13 +0100

    Inital commit.

---
+++ src/main/java/org/ual/aas/tasklists/App.java
@@ -0,0 +1,13 @@
+package org.ual.aas.tasklists;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
