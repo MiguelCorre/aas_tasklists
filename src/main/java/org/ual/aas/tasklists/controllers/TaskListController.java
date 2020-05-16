940c9c54e2a9344d4c5fc13c426667c3d0707f92
MiguelCorre <miguelahpc@hotmail.com>
2020-05-16 20:21:35 +0100

    Add controller.

---
+++ src/main/java/org/ual/aas/tasklists/controllers/TaskListController.java
@@ -0,0 +1,12 @@
+package org.ual.aas.tasklists.controllers;
+
+import java.util.List;
+
+import org.ual.aas.tasklists.models.TaskList;
+
+public class TaskListController {
+	public static List<TaskList> getLists(){
+		// TODO: obter as listas da base de dados
+		return null;
+	}
+}
