36f4490f53ed5820045dd25725137fda4ee01b4b
MiguelCorre <miguelahpc@hotmail.com>
2020-05-18 14:49:32 +0100

    ...

--- src/main/java/org/ual/aas/tasklists/views/CLI.java
+++ src/main/java/org/ual/aas/tasklists/views/CLI.java
@@ -17,36 +17,61 @@ import org.ual.aas.tasklists.models.TaskList;
 public class CLI {
     
     public void menuView() {
-          
-        System.out.println("Command Options: ");
-        System.out.println("1: Criaçao da base de dados");
-        System.out.println("2: Ver base de dados");
-        System.out.println("3: All Numbers Are the Same");
-        System.out.println("4: Sum Between Two Integers");
-        System.out.println("5: Repeat the String");
-        System.out.println("6: It is Palindrome");
-        System.out.println("7: Display");
-        System.out.println("8: Quit");
-        System.out.print("Your choice: ");
         
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                createDatabase(); // ESTA A FUNCIONAR MAS SE CORRER VARIAS VEZES FICAMOS COM COPIAS DE TASKS QUE NAO E SUPOSTO.
-                break;
-            case 2:
-                seeDatabase(1); // ESTA A FUNCIONAR MAS POR ALGUMA RAZAO APAGA A TABELA TASKLIST_TASK E ACHO QUE NAO E BOM.
-                break;
-            case 3:
-                // Perform "decrypt number" case.
-                break;
-            case 4:
-                
-                break;
-            default:
-                // The user input an unexpected choice.
-        }
+        while(true) {
+            System.out.println("Command Options: ");
+            System.out.println("1: Criaçao da base de dados");
+            System.out.println("2: Ver base de dados");
+            System.out.println("3: All Numbers Are the Same");
+            System.out.println("4: Sum Between Two Integers");
+            System.out.println("5: Repeat the String");
+            System.out.println("6: It is Palindrome");
+            System.out.println("7: Display");
+            System.out.println("8: Quit");
+            System.out.print("Your choice: ");
+
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    createDatabase(); 
+                    break;
+                case 2:
+                    readEntity(1); 
+                    break;
+                case 3:
+                    saveOrUpdateTasks(1);
+                    break;
+                case 4:
+                    deleteTasks(1);
+                    break;
+                case 5:
+                    insertTasks();
+                    break;
+                case 6:
+                    System.out.println("Command Options: ");
+                    System.out.println("1: Criaçao da base de dados");
+                    System.out.println("2: Ver base de dados");
+                    System.out.println("8: Quit");
+                    System.out.print("Your choice: ");
+
+                    int choice2 = scanner.nextInt();
+                    switch (choice2) {
+                        case 1:
+                            System.out.println("Workded");
+                            break;
+                        case 2:
+                            System.out.println("Worekd");
+                            break;
+                    }
+                    break;
+                case 7:
+                    System.exit(0);
+                default:
+                    // The user input an unexpected choice.
+            }
+            }
+        
     }
     
     public void createDatabase() {
@@ -54,12 +79,13 @@ public class CLI {
         taskList.getTasks().add(new Task("1st task", "doing"));
         taskList.getTasks().add(new Task("2nd task", "doing"));
         taskList.getTasks().add(new Task("3rd task", "doing"));
+        
         Transaction transaction = null;
         try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
             // start a transaction
             transaction = session.beginTransaction();
 //            // save the student objects
-            session.save(taskList);
+            session.saveOrUpdate(taskList);
 //            // commit transaction
             transaction.commit();
         } catch (Exception e) {
@@ -105,19 +131,16 @@ public class CLI {
 //        sessionFactory.close();
     }
     
-    public void seeDatabase(int id) {
+    public void readEntity(int id) {
         Transaction transaction = null;
-        try ( Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             // start a transaction
             transaction = session.beginTransaction();
 
             // get Student entity using get() method
             TaskList taskList = session.get(TaskList.class, id);
-            Task task = session.get(Task.class, id);
-
-            System.out.println(task.getDescription() + " FUNCIONOUUUUUUUUUUUUUUU");
-
-            System.out.println(taskList.getName() + " FUNCIONOU!!!!!!!");
+            System.out.println(taskList.getName());
+            //System.out.println(taskList.getEmail());
 
             // commit transaction
             transaction.commit();
@@ -127,6 +150,7 @@ public class CLI {
             }
             e.printStackTrace();
         }
+    }
 //        session = sessionFactory.openSession();
 //        session.beginTransaction();
 //
@@ -137,6 +161,87 @@ public class CLI {
 //
 //        session.getTransaction().commit();
 //        session.close();
+    
+    public static void saveOrUpdateTasks(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            //session.saveOrUpdate(taskList);
+
+            // get entity from database
+            TaskList taskList = session.get(TaskList.class, id);
+
+            // do changes 
+            taskList.setName("Ram");
+
+            // update the student object
+            session.saveOrUpdate(taskList);
+            
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
     
-}
+    public void deleteTasks(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // Delete a persistent object
+            TaskList taskList = session.get(TaskList.class, id);
+            if (taskList != null) {
+                session.delete(taskList);
+                System.out.println("student 1 is deleted");
+            }
+
+            // Delete a transient object
+//            Student student2 = new Student();
+//            student2.setId(2);
+//            session.delete(student2);
+//            System.out.println("Student 2 is deleted");
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    
+    public void insertTasks() {
+        //TaskList taskList = new TaskList("TaskListTest");
+        
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            
+            TaskList taskList = session.get(TaskList.class, 2);
+            taskList.getTasks().add(new Task("TestTask", "Test"));
+            // save the student objects
+            session.saveOrUpdate(taskList);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    }
+    
+    
+    
+
